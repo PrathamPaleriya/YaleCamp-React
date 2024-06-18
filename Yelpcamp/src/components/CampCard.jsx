ab06a1c948b25f73badccad30344f9553dc1cf14
Pratham Paleriya <prathampaleriya@gmail.com>
2024-06-19 00:11:32 +0530

    color changes of campcard

--- Yelpcamp/src/components/CampCard.jsx
+++ Yelpcamp/src/components/CampCard.jsx
@@ -4,11 +4,11 @@ import { Link } from 'react-router-dom'
 
 function CampCard({id, title, description, coverImage}) {
   return (
-    <div className='border-2 border-cream m-5 p-4 rounded-md'>
+    <div className='border-2 border-cream-outline m-5 p-4 rounded-md'>
       <img src={coverImage} alt="" className='object-cover w-full object-center'/>
       <h1 className='font-bold my-2 text-xl'>{title}</h1>
       <p className='text-cream-dark my-2 text-lg'>{description}</p>
-      <Link to={`/home/camp/${id}`} className='border-2 border-cream px-9 py-2 w-full block text-center mt-2 rounded-lg font-semibold hover:border-cream-dark '><button>View Campground</button></Link>
+      <Link to={`/home/camp/${id}`} className='border-2 border-cream-outline px-9 py-2 w-full block text-center mt-2 rounded-lg font-semibold hover:border-cream-dark '><button>View Campground</button></Link>
       
     </div>
   )
