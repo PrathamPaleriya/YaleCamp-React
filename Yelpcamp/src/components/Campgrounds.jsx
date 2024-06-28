e6012f33a2c57028618a48f05ba28d7be9c4d3ab
Pratham Paleriya <prathampaleriya@gmail.com>
2024-06-28 23:45:18 +0530

    seach box correction in campground

--- Yelpcamp/src/components/Campgrounds.jsx
+++ Yelpcamp/src/components/Campgrounds.jsx
@@ -5,11 +5,11 @@ import HeroCampDisplay from './HeroCampDisplay'
 function Campgrounds() {
   return (
     <div>
-        <div className='bg-cream px-20 rounded-md py-3 mx-10 my-5'>
+        <div className='bg-cream px-5 md:px-10 lg:px-20 rounded-md py-3 lg:mx-10 my-5'>
            <Searchbox placeholder="Search the Campgrounds"/> 
         </div>
         
-        <div className='relative mx-auto my-5 w-[85%]'>
+        <div className='relative mx-auto my-5 full lg:w-[85%]'>
             <h1 className='text-center font-bold text-3xl mt-20'>All Campgrounds</h1>
             <HeroCampDisplay/>
         </div>
